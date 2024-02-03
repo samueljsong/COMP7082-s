cf61ec58f691163b8f8291175eda88c611edf5e3
A1exander-liU <alexanderliu2016@gmail.com>
2024-02-03 12:37:42 -0800

    ci: exclude node_modules from bundle

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: {
     app: './src/index.ts',
   },
   target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: '/node_modules/' }],
   },
