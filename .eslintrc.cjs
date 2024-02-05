6ab0c0cfd5cc843041ba324f5a7851174b8b1f51
A1exander-liU <alexanderliu2016@gmail.com>
2024-02-04 18:20:30 -0800

    chore: update eslint and tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,15 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'prettier'],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+  },
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
