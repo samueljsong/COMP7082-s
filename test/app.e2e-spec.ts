19f89ecaf07948ed2c55a90136d42883171a637b
A1exander-liU <alexanderliu2016@gmail.com>
2024-02-04 18:23:57 -0800

    refactor: update to use new app

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,31 @@
 import request from 'supertest';
-import app from '../src/app';
+import App from '../src/app';
+import { HealthRoute } from '../src/health/health.route';
+import { AppRoute } from '../src/app/app.route';
 
 describe('App E2E', () => {
+  let app: App;
+
+  beforeAll(() => {
+    app = new App([new AppRoute(), new HealthRoute()], '/api');
+  });
+
+  describe('App', () => {
+    it('should be defined', () => {
+      expect(app).toBeDefined();
+    });
+  });
+
   describe('Going to app', () => {
     it('should respond with 200', async () => {
-      const res = await request(app).get('/');
+      const res = await request(app.server).get('/api');
       expect(res.statusCode).toBe(200);
     });
   });
 
-  describe('Health check', () => {
+  describe('Health Check', () => {
     it('should responsd with 200', async () => {
-      const res = await request(app).get('/health');
+      const res = await request(app.server).get('/health');
       expect(res.statusCode).toBe(200);
     });
   });
