670199ae5c554cfac7684bd86f95e265f049da9a
A1exander-liU <alexanderliu2016@gmail.com>
2024-02-05 22:24:09 -0800

    feat: configure PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,4 +2,14 @@ import { PrismaClient } from '@prisma/client';
 import { Service } from 'typedi';
 
 @Service()
-export class PrismaServce extends PrismaClient {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+}
