115bf78105a73ceaac20991c0ea279a89652dc9f
A1exander-liU <alexanderliu2016@gmail.com>
2024-02-10 19:07:11 -0800

    feat: update prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,4 +2,14 @@ import { PrismaClient } from '@prisma/client';
 import { Service } from 'typedi';
 
 @Service()
-export class PrismaServce extends PrismaClient {}
+export class PrismaServce extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+}
