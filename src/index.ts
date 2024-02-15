f32a7cc91965fffa0aaf2654ef2d1f96fbae6848
Jason Lui <jason53lui@gmail.com>
2024-02-15 00:34:51 -0800

    fixed the admin routing

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import App from './app';
 import { AppController } from './app/app.controller';
 import { HealthController } from './health/health.controller';
 import { AuthController } from './auth/auth.controller';
+import { AdminController } from './admin/admin.controller';
 
-const app = new App([AppController, HealthController, AuthController]);
+
+const app = new App([AppController, HealthController, AuthController, AdminController]);
 app.listen();
