6ac9dbaf5a375f699e44d5a9c06e601f19a05448
A1exander-liU <alexanderliu2016@gmail.com>
2024-02-15 16:42:03 -0800

    fix: move dotenv import so testing have access to env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import 'dotenv/config';
-
 import App from './app';
 import { AppController } from './app/app.controller';
 import { HealthController } from './health/health.controller';
 import { AuthController } from './auth/auth.controller';
 import { AdminController } from './admin/admin.controller';
-
 
 const app = new App([AppController, HealthController, AuthController, AdminController]);
 app.listen();
