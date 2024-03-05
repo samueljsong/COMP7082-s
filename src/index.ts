449de54cf5115fbbd16c2b5f3a39aaef815ae332
Jason Lui <jason53lui@gmail.com>
2024-03-04 22:30:07 -0800

    implemented some user routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { AppController } from './app/app.controller';
 import { HealthController } from './health/health.controller';
 import { AuthController } from './auth/auth.controller';
 import { AdminController } from './admin/admin.controller';
+import { UserController } from './user/user.controller';
 
-const app = new App([AppController, HealthController, AuthController, AdminController]);
+const app = new App([AppController, HealthController, AuthController, AdminController, UserController]);
 app.listen();
