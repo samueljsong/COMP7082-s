3008e93b5240bf91e159039973a9132edd6d47aa
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-07 14:11:27 -0800

    fix: use new decorators

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,10 @@
-import Container, { Service } from 'typedi';
+import { Service } from 'typedi';
 import { PrismaService } from '../prisma/prisma.service';
 
 // WHERE TO WRITE THE QUERIES
 @Service()
 export class UserService {
-  private readonly prisma = Container.get(PrismaService);
+  constructor(private readonly prisma: PrismaService) {}
 
   public allUsers() {
     return this.prisma.user.findMany();
