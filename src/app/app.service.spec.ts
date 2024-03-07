518319c2d819eabcf2d3f5241048bb7df9c92ced
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-07 08:26:24 -0800

    test: add app service tests

---
+++ src/app/app.service.spec.ts
@@ -0,0 +1,32 @@
+import { describe, expect, beforeAll, it, vi, test } from 'vitest';
+import { AppService } from './app.service';
+
+describe('GET /api', () => {
+  const app = new AppService();
+
+  beforeAll(() => {
+    vi.restoreAllMocks();
+  });
+
+  test('app service should be defined', () => {
+    expect(app).toBeDefined();
+  });
+
+  describe('welcome', () => {
+    it('should return Welcome!', () => {
+      const data = app.welcome();
+
+      expect(data).toEqual('Welcome!');
+    });
+  });
+
+  describe('greet', () => {
+    it('should return greeting with provided name', () => {
+      const name = 'alex';
+      const data = app.greet(name);
+
+      expect(data).toContain(name);
+      expect(data).toEqual(`Hello ${name}!`);
+    });
+  });
+});
