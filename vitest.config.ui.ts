faaf33bf48a6beadf331ed3321a2ad3a5fb2b1a2
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-07 13:58:46 -0800

    feat: update configs

--- vitest.config.ui.ts
+++ vitest.config.ui.ts
@@ -2,15 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['src/**/*.spec.ts', 'test/**/*.e2e-{test,spec}.{ts,js}'],
     coverage: {
       include: ['src/**/*.ts'],
       enabled: true,
-      thresholds: {
-        lines: 30,
-        branches: 30,
-        statements: 30,
-        functions: 30,
-      },
     },
   },
 });
