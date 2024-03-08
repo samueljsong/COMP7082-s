e600f1bcfd3ce7e210cb41e279b4f0a3fe239f7a
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-07 20:00:09 -0800

    test: update test configs

--- vitest.config.coverage.ts
+++ vitest.config.coverage.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['src/**/*.spec.ts', 'test/**/*.e2e-{test,spec}.{ts,js}'],
     coverage: {
-      include: ['src/**/*.ts'],
+      include: ['src/**/*.{controller,service,middleware}.ts'],
+      exclude: ['src/**/__mocks__'],
       enabled: true,
       thresholds: {
         lines: 30,
