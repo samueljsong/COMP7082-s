f1d9de92c3fb1f62b42f7144b7c342fed4e4a4ab
Jason Lui <jason53lui@gmail.com>
2024-03-09 16:51:08 -0800

    finished the user.conroller.spec.ts

--- src/user/tests/user.controller.spec.ts
+++ src/user/tests/user.controller.spec.ts
@@ -2,17 +2,31 @@ import { describe, it, expect, beforeEach } from 'vitest';
 import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
 import prisma from '../../prisma/__mocks__/prisma.service';
+import { user } from '@prisma/client';
 
 describe('UserController', () => {
     let controller: UserController;
     let userService: UserService;
 
-    // const fakeReport = {
-    //     locationTagId: 101,
-    //     subject: "TESTING SUBJECT",
-    //     description: "TESTINGING DESCRIPTION",
-    //     cloudinaryUrl: "https://res.cloudinary.com/dxp9ftmcw/image/upload/v1709930592/orintjn3fxpkwbwtv9fp.png"
-    // }
+    const fakeReport = {
+        title: "TESTING TITLE",
+        date_submitted: new Date(),
+        statusId: 1,
+        locationTagId: 101,
+        subject: "TESTING SUBJECT",
+        description: "TESTINGING DESCRIPTION",
+        image_id: 14,
+        cloudinaryUrl: "https://res.cloudinary.com/dxp9ftmcw/image/upload/v1709930592/orintjn3fxpkwbwtv9fp.png"
+    }
+
+    const adminUser: user = {
+        user_id: 16,
+        user_type_id: 2,
+        email: 'jasonlui40@my.bcit.ca',
+        hashed_password: '$2a$10$aP7vsjweB9VHe/a8338.hupLw9k/IC76WX9wAzdnXbFcMiR9hUTw2',
+        first_name: 'Jason',
+        last_name: 'Lui',
+      };
 
     beforeEach(() => {
         userService = new UserService(prisma);
@@ -59,9 +73,45 @@ describe('UserController', () => {
         expect(result).toStrictEqual(expected);
     });
 
-    // it('should create a report', async () => {
-    //     const result = await controller.createUserReport(fakeReport, );
-    //     expect(result).toBe(true);
-    // });
+    it('should create a report', async () => {
+        const expected = {
+            "statusCode": 200,
+            "message": "Successfully created report"
+        };
+
+        const request = {
+                user: {
+                    user_id: adminUser.user_id,
+                    email: adminUser.email,
+                    first_name: adminUser.first_name,
+                    last_name: adminUser.last_name,
+                    user_type: 'admin',
+                },
+            } as unknown as Request;
+            prisma.report.create.mockResolvedValueOnce({
+                report_id: 14,
+                title: fakeReport.title,
+                description: fakeReport.description,
+                date_submitted: fakeReport.date_submitted,
+                location_tag_id: fakeReport.locationTagId,
+                status_id: fakeReport.statusId,
+                user_id: adminUser.user_id,
+            });
+
+            prisma.image.create.mockResolvedValueOnce({ 
+                image_id: 14,
+                cloudinary_id: fakeReport.cloudinaryUrl
+            });
+
+            prisma.report_image.create.mockResolvedValueOnce({
+                report_id: 34,
+                image_id: 14,
+                report_image_id: 14
+                
+            });
+
+        const result = await controller.createUserReport(fakeReport, request);
+        expect(result).toStrictEqual(expected);
+    });
 
 });
