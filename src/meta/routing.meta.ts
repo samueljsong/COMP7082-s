a68da656738dec6c09f9e92fc60347e2f2e719f3
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-11 20:01:12 -0700

    feat: add service decorator
    wrap over tsyringe

--- src/meta/routing.meta.ts
+++ src/meta/routing.meta.ts
@@ -1,9 +1,23 @@
 import { Controller } from 'routing-controllers';
-import { injectable } from 'tsyringe';
+import { injectable, singleton } from 'tsyringe';
 
 export function ServiceController(...args: Parameters<typeof Controller>) {
   return (target: new (...args: any[]) => any) => {
     injectable()(target);
     Controller(...args)(target);
+  };
+}
+
+export interface ServiceOptions {
+  scope: 'transient' | 'global';
+}
+
+export function Service(options: ServiceOptions = { scope: 'transient' }) {
+  return (target: new (...args: any[]) => any) => {
+    if (options.scope === 'transient') {
+      injectable()(target);
+    } else {
+      singleton()(target);
+    }
   };
 }
