44c4f7164231d13cbe73d31e3db58b8babfdea08
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-12 13:16:13 -0700

    test:integration: login and me route when ok

--- src/tests/auth.spec.ts
+++ src/tests/auth.spec.ts
@@ -13,11 +13,31 @@ describe('auth tests', () => {
   it('should response with 200 on successful login', async () => {
     const res = await request(app.server)
       .post('/api/auth/login')
-      .send({ email: 'aliu142@my.bcit.ca', password: 'asd' });
+      .send({ email: 'test1@my.bcit.ca', password: 'test1' });
 
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveProperty('statusCode', 200);
     expect(res.body).toHaveProperty('message', 'Successful Login');
     expect(res.headers['set-cookie'].length).toBeGreaterThan(0);
+  });
+
+  it('should get user info after successful login', async () => {
+    const testOneUser = {
+      user_id: 1,
+      email: 'test1@my.bcit.ca',
+      first_name: 'test1',
+      last_name: 'test1',
+      user_type: 'member',
+    };
+    const loginRes = await request(app.server)
+      .post('/api/auth/login')
+      .send({ email: 'test1@my.bcit.ca', password: 'test1' });
+    expect(loginRes.statusCode).toBe(200);
+
+    const cookie = loginRes.get('Set-Cookie');
+
+    const meRes = await request(app.server).get('/api/auth/me').set('Cookie', cookie);
+    expect(meRes.statusCode).toBe(200);
+    expect(meRes.body).toStrictEqual({ statusCode: 200, message: 'User details', ...testOneUser });
   });
 });
