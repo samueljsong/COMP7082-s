f7c9e011cbad761ed6884f6150e8e97a84c3093d
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-11 18:49:49 -0700

    test: add setup script for integration tests

--- src/tests/helpers/setup.ts
+++ src/tests/helpers/setup.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { beforeEach } from 'vitest';
 
-async function resetDb() {
-  const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
+async function resetDb() {
   await prisma.$transaction([prisma.report_image.deleteMany(), prisma.image.deleteMany(), prisma.report.deleteMany()]);
 }
 
