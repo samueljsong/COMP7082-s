bb39b87dd0dc453a9a812322798c959f5b8b11fd
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-17 14:13:26 -0700

    test: add setup for ui:test

--- vitest.config.ui.ts
+++ vitest.config.ui.ts
@@ -4,6 +4,7 @@ import vitestConfig from './vitest.config';
 export default mergeConfig(vitestConfig, {
   test: {
     include: ['src/**/*.spec.ts'],
+    setupFiles: ['src/tests/helpers/setup.ts'],
     coverage: {
       include: ['src/**/*.{controller,service,middleware}.ts'],
       exclude: ['src/**/__mocks__'],
