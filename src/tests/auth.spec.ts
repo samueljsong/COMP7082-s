aa4f1528c834f7e03bb76a1f9bd07feaa6f8e6e1
A1exander-liU <alexanderliu2016@gmail.com>
2024-03-17 13:28:45 -0700

    test(auth): check diff inputs

--- src/tests/auth.spec.ts
+++ src/tests/auth.spec.ts
@@ -24,69 +24,121 @@ const testOneUser = {
 describe('auth tests', () => {
   const app = new App([AppController, HealthController, AuthController, AdminController, UserController]);
 
-  it('should response with 200 on successful login', async () => {
-    const res = await request(app.server)
-      .post('/api/auth/login')
-      .send({ email: 'test1@my.bcit.ca', password: 'test1' });
+  describe('login', () => {
+    it('should response with 200 correct credentials', async () => {
+      const res = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'test1@my.bcit.ca', password: 'test1' });
 
-    expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty('statusCode', 200);
-    expect(res.body).toHaveProperty('message', 'Successful Login');
-    expect(res.headers['set-cookie']?.length).toBeGreaterThan(0);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('statusCode', 200);
+      expect(res.body).toHaveProperty('message', 'Successful Login');
+      expect(res.headers['set-cookie']?.length).toBeGreaterThan(0);
+    });
+
+    it('should respond with 401 if user does not exist', async () => {
+      const loginRes = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'random1@my.bcit.ca', password: 'random1' });
+      expect(loginRes.status).toBe(400);
+      expect(loginRes.body).toHaveProperty('message');
+      expect(loginRes.body.message).toMatch(/user does not exist/i);
+    });
+
+    it('should respond with 400 when password is not string', async () => {
+      const loginRes = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'test1@my.bcit.ca', password: 123 });
+      expect(loginRes.status).toBe(400);
+      expect(loginRes.body).toHaveProperty('message');
+      expect(loginRes.body.message).toMatch(/password must be a string/);
+    });
+
+    it('should respond with 400 when email and password dont exist', async () => {
+      const loginRes = await request(app.server).post('/api/auth/login').send({});
+      expect(loginRes.status).toBe(400);
+      expect(loginRes.body).toHaveProperty('message');
+      expect(loginRes.body.message).toMatch(/must be an email|password should be empty|password must be a string/);
+    });
+
+    it('should respond with 400 when email and password is empty', async () => {
+      const loginRes = await request(app.server).post('/api/auth/login').send({ email: '', password: '' });
+      expect(loginRes.status).toBe(400);
+      expect(loginRes.body).toHaveProperty('message');
+      expect(loginRes.body.message).toMatch(/must be an email|password should be empty/);
+    });
+
+    it('should respond with 400 when email is not proper email', async () => {
+      const loginRes = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'test.my.bcit.ca', password: 'test1' });
+      expect(loginRes.status).toBe(400);
+      expect(loginRes.body).toHaveProperty('message');
+      expect(loginRes.body.message).toMatch(/must be an email/);
+    });
   });
 
-  it('should get user info after successful login', async () => {
-    const loginRes = await request(app.server)
-      .post('/api/auth/login')
-      .send({ email: 'test1@my.bcit.ca', password: 'test1' });
-    expect(loginRes.status).toBe(200);
+  describe('me', () => {
+    it('should get user info after successful login', async () => {
+      const loginRes = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'test1@my.bcit.ca', password: 'test1' });
+      expect(loginRes.status).toBe(200);
 
-    const cookie = loginRes.get('Set-Cookie');
+      const cookie = loginRes.get('Set-Cookie');
 
-    const meRes = await request(app.server).get('/api/auth/me').set('Cookie', cookie);
-    expect(meRes.status).toBe(200);
-    expect(meRes.body).toStrictEqual({ statusCode: 200, message: 'User details', ...testOneUser });
+      const meRes = await request(app.server).get('/api/auth/me').set('Cookie', cookie);
+      expect(meRes.status).toBe(200);
+      expect(meRes.body).toStrictEqual({ statusCode: 200, message: 'User details', ...testOneUser });
+    });
   });
 
-  it('should respond with 400 when password is not string', async () => {
-    const loginRes = await request(app.server)
-      .post('/api/auth/login')
-      .send({ email: 'test1@my.bcit.ca', password: 123 });
-    expect(loginRes.status).toBe(400);
-  });
+  describe('logout', () => {
+    it('should logout successfully when logged in', async () => {
+      const loginRes = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'test1@my.bcit.ca', password: 'test1' });
+      expect(loginRes.statusCode).toBe(200);
 
-  it('should logout successfully when logged in', async () => {
-    const loginRes = await request(app.server)
-      .post('/api/auth/login')
-      .send({ email: 'test1@my.bcit.ca', password: 'test1' });
-    expect(loginRes.statusCode).toBe(200);
+      const cookie = loginRes.get('Set-Cookie');
 
-    const cookie = loginRes.get('Set-Cookie');
+      const logoutRes = await request(app.server).get('/api/auth/logout').set('Cookie', cookie);
+      expect(logoutRes.statusCode).toBe(200);
+      expect(logoutRes.body).toStrictEqual({ statusCode: 200, message: 'Logout' });
+    });
 
-    const logoutRes = await request(app.server).get('/api/auth/logout').set('Cookie', cookie);
-    expect(logoutRes.statusCode).toBe(200);
-    expect(logoutRes.body).toStrictEqual({ statusCode: 200, message: 'Logout' });
-  });
+    it('should respond with 401 when logging out again with same token', async () => {
+      const expectedResponse = {
+        statusCode: StatusCodes.UNAUTHORIZED,
+        error: ReasonPhrases.UNAUTHORIZED,
+        message: 'Blacklisted token',
+      };
 
-  it('should respond with 401 when logging out again with same token', async () => {
-    const expectedResponse = {
-      statusCode: StatusCodes.UNAUTHORIZED,
-      error: ReasonPhrases.UNAUTHORIZED,
-      message: 'Blacklisted token',
-    };
+      const loginRes = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'test1@my.bcit.ca', password: 'test1' });
+      expect(loginRes.statusCode).toBe(200);
 
-    const loginRes = await request(app.server)
-      .post('/api/auth/login')
-      .send({ email: 'test1@my.bcit.ca', password: 'test1' });
-    expect(loginRes.statusCode).toBe(200);
+      const cookie = loginRes.get('Set-Cookie');
 
-    const cookie = loginRes.get('Set-Cookie');
+      const logoutRes = await request(app.server).get('/api/auth/logout').set('Cookie', cookie);
+      expect(logoutRes.statusCode).toBe(200);
 
-    const logoutRes = await request(app.server).get('/api/auth/logout').set('Cookie', cookie);
-    expect(logoutRes.statusCode).toBe(200);
+      const failedLogoutRes = await request(app.server).get('/api/auth/logout').set('Cookie', cookie);
+      expect(failedLogoutRes.statusCode).toBe(401);
+      expect(failedLogoutRes.body).toStrictEqual(expectedResponse);
+    });
 
-    const failedLogoutRes = await request(app.server).get('/api/auth/logout').set('Cookie', cookie);
-    expect(failedLogoutRes.statusCode).toBe(401);
-    expect(failedLogoutRes.body).toStrictEqual(expectedResponse);
+    it('should respond with 401 if token not provided', async () => {
+      const loginRes = await request(app.server)
+        .post('/api/auth/login')
+        .send({ email: 'test1@my.bcit.ca', password: 'test1' });
+      expect(loginRes.statusCode).toBe(200);
+
+      const logoutRes = await request(app.server).get('/api/auth/logout');
+      expect(logoutRes.statusCode).toBe(401);
+      expect(logoutRes.body).toHaveProperty('message');
+      expect(logoutRes.body.message).toMatch(/token not provided/i);
+    });
   });
 });
