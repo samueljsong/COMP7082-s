ba3bff24c33f60b25353532cfa345c8e70907bbd
A1exander-liU <alexanderliu2016@gmail.com>
2024-04-03 23:02:43 -0700

    test: use verbose output

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -5,6 +5,7 @@ export default mergeConfig(vitestConfig, {
   // root level test directory contains all e2e tests
   test: {
     include: ['test/**/*.e2e-{test,spec}.{ts,js}'],
+    reporters: ['verbose'],
     setupFiles: ['src/tests/helpers/e2e.setup.ts'],
   },
 });
--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -4,5 +4,6 @@ import vitestConfig from './vitest.config';
 export default mergeConfig(vitestConfig, {
   test: {
     include: ['src/**/*.spec.ts', '!src/tests/**/*.spec.ts'],
+    reporters: ['verbose'],
   },
 });
